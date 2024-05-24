70e564a0105bd3fd4df435ed875d8353666f9e04
Leo Oh <leooh29@gmail.com>
2024-05-25 01:24:06 +0800

    add stop code to audio encode/decode

--- cheng/steganography.py
+++ cheng/steganography.py
@@ -22,7 +22,7 @@ class Steganography:
                         
         elif(input_path.endswith(('.wav', '.mp3', '.ogg', '.flac', '.m4a', '.aac'))):
             print("Encoding message into audio...")
-            return Steganography.alt_encode_audio(input_path, msg, lsb, output_dir)
+            return Steganography.encode_audio(input_path, msg, lsb, output_dir)
         
         elif(input_path.endswith(('.mp4', '.avi', '.mov', '.mkv'))):
             print("Encoding message into video file...")
@@ -43,7 +43,7 @@ class Steganography:
                         
         elif(input_path.endswith(('.wav', '.mp3', '.ogg', '.flac', '.m4a', '.aac'))):
             print("Decoding message from audio...")
-            return Steganography.alt_decode_audio(input_path, lsb)
+            return Steganography.decode_audio(input_path, lsb)
         
         elif(input_path.endswith(('.mp4', '.avi', '.mov', '.mkv'))):
             print("Decoding message from video file...")
@@ -64,7 +64,7 @@ class Steganography:
 
         width, height = img.size
         stop_code = '\x00'
-        max_payload_char = math.floor((width * height * 3 * lsb) / 8) - len(stop_code);
+        max_payload_char = math.floor((width * height * 3 * lsb) / 8) - len(stop_code)
         print("Maximum number of payload characters for the current image: " + str(max_payload_char))
         msg += stop_code # add a stopping null character, one char space should be reserved for stopping code
         length = len(msg)
@@ -151,7 +151,6 @@ class Steganography:
         Use the LSB of the pixels to decode the message from the image
         """
         try:
-            stopping_code = "====="
             fileExt = img_path.split('.')[-1]
             if fileExt == '.gif' or fileExt == '.GIF':
                 img = Image.open(img_path).convert("RGB")
@@ -167,9 +166,6 @@ class Steganography:
                     r, g, b, a  = img.getpixel((col, row))
                     
                     # Extract the bits of the message
-                    # msg += bin(r)[-lsb:]  # Red channel
-                    # msg += bin(g)[-lsb:]  # Green channel
-                    # msg += bin(b)[-lsb:]  # Blue channel
                     r &= mask
                     bin = format(r, 'b').rjust(lsb, '0')
                     msg_bin += bin
@@ -190,10 +186,8 @@ class Steganography:
                 if len(byte) == 8:
                     if int(byte, 2) == 0: # break if we hit a null character (stop code)
                         break
-                    
                     char = chr(int(byte, 2))
                     decoded_msg += char
-            #decoded_msg = ''.join(chr(int(msg[i:i+8], 2)) for i in range(0, len(msg), 8))
 
             #print("decoded_msg with stopping code: " + decoded_msg)
             #print("decoded_msg without stopping code: " + decoded_msg[:stopping_code_position])
@@ -214,97 +208,15 @@ class Steganography:
         
         # read wave audio file
         song = wave.open(audio_path, mode='rb')
-        # Read frames and convert to byte array
-        frame_bytes = bytearray(list(song.readframes(song.getnframes())))
-
-        # Check if message is too long for audio
-        if len(msg) > 255:
+        
+        # add stop code char to signify end of secret msg
+        stop_char = '='
+        msg += stop_char
+        
+        # check if cover audio file is large enough for payload msg
+        if (len(msg) * 8) > song.getnframes():
             return {"status": False, "message": "The message is too long for the audio file"}
-
-        # The "secret" text message
-        string = msg
-        # Append dummy data to fill out rest of the bytes. Receiver shall detect and remove these characters.
-        string = string + int((len(frame_bytes)-(len(string)*8*8))/8) *'#'
         
-        # Convert text to bit array
-        totalBinStr = ''
-        for eachChar in string:
-            eachCharInt = ord(eachChar)
-            eachCharBinStr = bin(eachCharInt)
-            # remove the '0b' from front and pads front with 0s until 8 bits
-            eachCharBinStr = eachCharBinStr.lstrip('0b').rjust(8,'0') 
-            totalBinStr += eachCharBinStr  
-        # map each binary string to the integer cast function, in effect casting every 8 bits of binary string into integer; '01010101' --> 01010101
-        MappedBinToInt = map(int, totalBinStr)
-        # put the integers into a list 
-        bits = list(MappedBinToInt)
-        
-        # so confusing...
-        # bits = list(map(int, ''.join([bin(ord(eachChar)).lstrip('0b').rjust(8,'0') for eachChar in string])))
-
-        # Replace LSB of each byte of the audio data by one bit from the text bit array
-        for i, bit in enumerate(bits):
-            frame_bytes[i] = (frame_bytes[i] & (255 - 2**lsb)) | bit
-        # Get the modified bytes
-        frame_modified = bytes(frame_bytes)
-
-        output_path = os.path.join(output_dir + '/encoded.' + fileExt)
-        # Write bytes to a new wave audio file
-        with wave.open(output_path, 'wb') as fd:
-            fd.setparams(song.getparams())
-            fd.writeframes(frame_modified)
-        song.close()
-        
-        # Handle the case where the output file is not created
-        try:
-            with wave.open(output_path, 'rb') as fd:
-                os.startfile(output_path)
-                return {"status": True, "message": "Audio encoded successfully"}
-        except FileNotFoundError:
-            return {"status": False, "message": "Error creating encoded audio file"}
-        except wave.Error:
-            return {"status": False, "message": "Error creating encoded audio file"}
-        
-        
-    @staticmethod
-    def decode_audio(audio_path, lsb):
-        """
-        Decode the hidden message from an audio file
-        """
-        try:
-            # Use wave package (native to Python) for reading the received audio file
-            import wave
-            song = wave.open(audio_path, mode='rb')
-            # Convert audio to byte array
-            frame_bytes = bytearray(list(song.readframes(song.getnframes())))
-
-            # Extract the LSB of each byte
-            extracted = list()
-            for i in range(len(frame_bytes)):
-                extracted.insert(0, frame_bytes[i] & (1 << lsb))
-            
-            # extracted = [frame_bytes[i] & (1 << lsb) for i in range(len(frame_bytes))]
-            
-            # Convert byte array back to string
-            string = "".join(chr(int("".join(map(str,extracted[i:i+8])),2)) for i in range(0,len(extracted),8))
-            # Cut off at the filler characters
-            decoded = string.split("###")[0]
-            song.close()
-            return {"status": True, "message": decoded}
-        except Exception as e:
-            return {"status": False, "message": str(e)}
-    
-    
-    @staticmethod
-    def alt_encode_audio(audio_path, msg, lsb, output_dir):
-        """
-        Use the LSB of the audio samples to encode the message into the audio file
-        """
-        # Get the file extension
-        fileExt = audio_path.split('.')[-1]
-        
-        # read wave audio file
-        song = wave.open(audio_path, mode='rb')
         # Read frames and convert to byte array
         frame_bytes = bytearray(list(song.readframes(song.getnframes())))
         
@@ -313,8 +225,8 @@ class Steganography:
         lenOfSecretBits = len(secretBits)
         
         # Check if message is too long for audio
-        if len(secretBits) > (len(frame_bytes) * 8):
-            return {"status": False, "message": "The message is too long for the audio file"}
+        # if len(secretBits) > (len(frame_bytes) * 8):
+        #     return {"status": False, "message": "The message is too long for the audio file"}
         
         # Encode payload into cover frame
         for i in range(0, lenOfSecretBits, lsb):
@@ -355,7 +267,7 @@ class Steganography:
         
         
     @staticmethod
-    def alt_decode_audio(audio_path, lsb):
+    def decode_audio(audio_path, lsb):
         """
         Decode the hidden message from an audio file
         """
@@ -374,6 +286,9 @@ class Steganography:
                 byte = byte & mask
                 bin = format(byte, 'b').rjust(lsb, '0')
                 payload_bin += bin
+                # if payload_bin[-8:] == "00111101": # stop_char '=' = 00111101
+                #     payload_bin = payload_bin[:-8]
+                #     break
             
             # Convert binary payload to text
             payload = ''
@@ -381,7 +296,10 @@ class Steganography:
                 byte = payload_bin[i:i+8]
                 if len(byte) == 8:
                     char = chr(int(byte, 2))
-                    payload += char
+                    if char is "=": # stop code reached
+                        break
+                    else:
+                        payload += char
             
             song.close()
             return {"status": True, "message": payload}
